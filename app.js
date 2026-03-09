def22cadebb7e50d8391f155c79808922343c5c9
Samuel Montalbano <120127489+Sammontalbano22@users.noreply.github.com>
2026-03-09 06:34:02 +0000

    final update and bug fix

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const fs = require('fs');
 const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 
 (async () => {
+  //I had to use --nosandbox and set and executable path to get this working because following the slides setup for ARM did not work.https://pptr.dev/troubleshooting
     // TODO: Launch a browser instance and open a new page
     const browser = await puppeteer.launch({
         headless: true,
         executablePath: '/usr/bin/chromium',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox'] //had to do this because of no good sandbox error. I think its because of the docker.
     });
     const page = await browser.newPage();
 
@@ -37,7 +38,7 @@ const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
         // TODO: Star the repository
         // HINT: Use selectors to identify and click on the star button
         await page.waitForSelector('form[action$="/star"]');
-        await page.evaluate(() => document.querySelector('form[action$="/star"]').submit());
+        await page.evaluate(() => document.querySelector('form[action$="/star"]').submit()); // I am calliing submit instead of click because i was having issues with it being a form.
         await new Promise(r => setTimeout(r, 1000)); // This timeout helps ensure that the action is fully processed
     }
 
@@ -49,7 +50,7 @@ const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
     await page.evaluate(() =>
         [...document.querySelectorAll('button[data-show-dialog-id]')]
             .find(btn => btn.innerText.includes('list')).click()
-    );
+    ); // This finds the button that contains "list" in its text and clicks it
 
     // TODO: Create a list named "Node Libraries"
     // HINT: Wait for the input field and type the list name
@@ -59,6 +60,9 @@ const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
     // Wait for buttons to become visible
     await new Promise(r => setTimeout(r, 1000));
 
+
+
+    //I had to stray here a bit because when testing the create list form, I noticed one click initiates validation and then it took another click to actually submit the form. So I am clicking twice with a delay in between to account for this behavior.
     // Click Create once to trigger validation
     const buttons = await page.$$('.Button--primary.Button--medium.Button');
     for (const button of buttons) {
@@ -96,11 +100,13 @@ const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
         await new Promise(r => setTimeout(r, 3000));
         await page.waitForSelector('.js-user-list-menu-item');
 
-        // Match by label text instead of value since value is now a numeric ID
+
+        //Waits for selector then evaluates to find what corresponds to Node Libraries.
         await page.evaluate(() => {
             const items = document.querySelectorAll('.form-checkbox');
             for (const item of items) {
                 const label = item.querySelector('.Truncate-text');
+                //Checks if the label text includes "Node Libraries" and clicks the corresponding checkbox
                 if (label && label.innerText.includes('Node Libraries')) {
                     item.querySelector('.js-user-list-menu-item').click();
                     break;
